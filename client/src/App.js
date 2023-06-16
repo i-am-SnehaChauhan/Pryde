b5419fce38663fb51d85278a5e3eb955b68c1cac
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2023-06-16 06:57:51 +0000

    React App Setup

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Login from './components/account/Login'
+function App() {
+  return (
+    <div className="App">
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
