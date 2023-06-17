723a8b922a23412da0d8d0e6cfb28994e398832f
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2023-06-17 11:37:17 +0530

    Login Page Done

--- client/src/components/account/Login.jsx
+++ client/src/components/account/Login.jsx
@@ -1,8 +1,61 @@
 import React from 'react'
+import { Box, TextField, Button, styled, Typography } from '@mui/material'
+import imageURL from '../../images/blog.png'
 
+
+const Component = styled(Box)`
+  width: 400px;
+  margin: auto;
+  margin-top: 80px;
+  justify-content: center;
+  box-shadow: 5px 0 10px 0 rgba(0,0,0,0.5);
+ 
+`
+const Image = styled('img')({
+  width: 500,
+  display: 'flex',
+  margin: 'auto',
+  padding: '10px',
+
+})
+
+const Wrapper = styled(Box)`
+   padding: 20px 35px;
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    & > div, & > button, & > p{
+      margin-top: 15px;
+    }
+`
+
+const SignUpButton = styled(Button)`
+  text-transform: none;
+  background-color: #fff;
+  color: #2874f0;
+  border-radius: 2px;
+  box-shadow: 0 2px 4px 0 rgb(0 0 0 / 20%);
+`
+
+const LoginButton = styled(Button)`
+  text-transform: none;
+`
 const Login = () => {
+
   return (
-    <h1>Login</h1>
+    <Component>
+      <Box>
+        <Image src={imageURL} alt="Login" />
+        <Wrapper>
+          <TextField variant="standard" />
+          <TextField variant="standard" />
+          <LoginButton variant="contained">Login</LoginButton>
+          <Typography style={{textAlign: 'center'}}>OR</Typography>
+          <SignUpButton>Create an Account</SignUpButton>
+        </Wrapper>
+      </Box>
+    </Component>
+
   )
 }
 
