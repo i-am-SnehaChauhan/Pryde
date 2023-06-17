5db164c157d245288f3f8f093fba9963c6276d87
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2023-06-17 12:13:32 +0530

    Commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/account/Login'
 import Home from './components/Home';
+import Forum from './components/forum/Forum';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/accounts' element={<Login />} />
+      <Route path='/blog' element={<Forum />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/components/forum/Forum.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Banner from '../banner/Banner'
+
+const Forum = () => {
+  return (
+    <div>
+      <Banner/>
+    </div>
+  )
+}
+
+export default Forum
