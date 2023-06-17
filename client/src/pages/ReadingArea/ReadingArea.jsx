490db63e6ecb4e57e1c573288c1d6c796a47b05a
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2023-06-17 23:55:05 +0530

    Reading Area Set up

---
+++ client/src/pages/ReadingArea/ReadingArea.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../../components/Header'
+import SearchBar from '../../components/SearchBar'
+
+const ReadingArea = () => {
+  return (
+    <div>
+      {/* Page Header */}
+      <Header />
+
+      {/* Search Bar */}
+      <SearchBar />
+
+      {/* Blog List */}
+      
+    </div>
+  )
+}
+
+export default ReadingArea
