aa9c9e3b959afc4121af051ca1cb4ef761a570d6
pooja-gera <55778515+pooja-gera@users.noreply.github.com>
2023-06-18 16:55:42 +0530

    minor fixes, reading Area complete

--- client/src/pages/Blog/index.jsx
+++ client/src/pages/Blog/index.jsx
@@ -21,7 +21,54 @@ const Blog = () => {
 
   return (
     <>
-      <Link className='blog-goBack' to='/'>
+     <nav className="nav bd-container">
+      <a href="/" className="nav__logo">
+        Pryde &#127987;&#65039;&#8205;&#127752;
+      </a>
+      <div className="nav__menu" id="nav-menu">
+        <ul className="nav__list">
+        <li className="nav__item">
+            <a href="#about" className="nav__link ">
+              About
+            </a>
+          </li>
+          {/* 
+          <li className="nav__item">
+            <a href="#theme" className="nav__link ">
+              Theme
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="#events" className="nav__link">
+              Events
+            </a>
+          </li> */}
+          {/* <li class="nav__item"><a href="#timeline" class="nav__link">Timeline</a></li> */}
+          <li className="nav__item">
+            <a href="#features" className="nav__link">
+             Features
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="#team" className="nav__link" >
+              Team
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="/accounts" className="nav__link">
+              Login
+            </a>
+          </li>
+          <li>
+            <i className="bx bx-toggle-left change-theme" id="theme-button" />
+          </li>
+        </ul>
+      </div>
+      <div className="nav__toggle" id="nav-toggle">
+        <i className="bx bx-grid-alt" />
+      </div>
+    </nav>
+      <Link className='blog-goBack' to='/readingArea'>
         <span> &#8592;</span> <span>Go Back</span>
       </Link>
       {blog ? (
